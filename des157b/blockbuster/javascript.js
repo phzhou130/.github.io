cc509a67bdd6e48cd7ac8112f39e25ca0474c2e4
Penghua Zhou <phzhou@ucdavis.edu>
2022-01-18 05:36:57 -0800

    new

--- des157b/blockbuster/javascript.js
+++ des157b/blockbuster/javascript.js
@@ -2,8 +2,6 @@
     'use strict';
     console.log('reading');
     const myVideo = document.querySelector('#myVideo');
-    const fs = document.querySelector('.fa-expand');
-    // add the loading icon variable here
     const line1 = document.querySelector('#line1');
     const line2 = document.querySelector('#line2');
     const line3 = document.querySelector('#line3');
@@ -15,7 +13,6 @@
         line: [line1, line2, line3,line4]
     }
 
-    // add the loading icon script here
 
     const intervalID = setInterval(checkTime, 1000);
     const loadingMsg = document.querySelector('#loading');
@@ -39,14 +36,6 @@
         myVideo.style.filter=`grayscale(${event.clientX-event.clientY-event.clientY}%)`;
     })
 
-    fs.addEventListener('click', function() {
-        // The fullscreenElement attribute returns null if the element is in windowed mode
-        if (!document.fullscreenElement) {
-            // document.documentElement returns the Element that is a direct child of the document, the <html> element
-            document.documentElement.requestFullscreen();
-        } else {
-            document.exitFullscreen();
-        }
-    });
+   
 
 })();
