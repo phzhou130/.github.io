dba5b366f13c9fb644bdd5723d49ef636f1fda41
Penghua Zhou <phzhou@ucdavis.edu>
2022-03-01 03:32:28 -0800

    new

--- des157b/version1/script.js
+++ des157b/version1/script.js
@@ -1,7 +1,7 @@
 const hero = document.querySelector('[data-hero]')
 
-const menuButton = document.querySelector('[data-btn="menu"]')
 const menu = document.querySelector('[data-menu]')
+const menuButton = document.querySelector('[data-btn="menu"]')
 
 
 menuButton.addEventListener('click', () => {
@@ -12,15 +12,16 @@ menuButton.addEventListener('click', () => {
 
 
 window.addEventListener('mousemove', (e) => {
-	const { clientX, clientY } = e
+  const { clientX, clientY } = e
   const x = Math.round((clientX / window.innerWidth) * 100)
   const y = Math.round((clientY / window.innerHeight) * 100)
-	
 	gsap.to(hero, {
     '--x': `${x}%`,
     '--y': `${y}%`,
     duration: 0.3,
     ease: 'sine.out',
   })
+
 })
+
 
